1a0aebe65315850e1a811f7e8b84e1a124625b2b
Vitalii Erenkov <v.yerenkov@redbeat.com>
2025-06-03 18:45:00 +0300

    Hometask_5_2

---
+++ lesson_05/homework_5_2.py
@@ -0,0 +1,29 @@
+people_records = [
+  ('John', 'Doe', 28, 'Engineer', 'New York'),
+  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+
+people_records.insert(0, ('Tom', 'Ford', 50, 'Actor', 'Philadelphia'))
+
+people_records[1], people_records[5] = people_records[5], people_records[1]
+print(people_records)
+
+index_to_check = [6, 10, 13]
+all_above_30 = all(people_records[i][2] >= 30 for i in index_to_check)
+
+print(f"\nЧи всі люди з індексами 6, 10, 13 мають вік ≥ 30: {all_above_30}")
+
+
